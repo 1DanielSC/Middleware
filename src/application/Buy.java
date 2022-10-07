014cdda2db81b2e00caf57edf51f9c36d477b191
1danielsc <danielscolao@gmail.com>
2022-10-06 21:07:12 -0300

    add: remote class example

--- src/application/Buy.java
+++ src/application/Buy.java
@@ -1,5 +1,24 @@
 package application;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+import annotations.PostMapping;
+import annotations.RequestMapping;
+
+@RequestMapping(name = "/buy")
 public class Buy {
+
     
+    @PostMapping(route = "/add")
+    public JsonObject add(JsonObject json){
+        JsonElement var = json.get("valor");
+        int valor = var.getAsInt();
+        valor += 5;
+        
+        JsonObject result = new JsonObject();
+        result.addProperty("valor", valor);
+
+        return result;
+    }
 }
