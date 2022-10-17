56eeaeda77dd96abd4e7880ae809d031ef6478c8
1danielsc <danielscolao@gmail.com>
2022-10-17 18:56:28 -0300

    add new class for testing

--- src/application/Buy.java
+++ src/application/Buy.java
@@ -3,6 +3,7 @@ package application;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
+import annotations.GetMapping;
 import annotations.PostMapping;
 import annotations.RequestMapping;
 
@@ -15,6 +16,16 @@ public class Buy {
         JsonElement var = json.get("valor");
         int valor = var.getAsInt();
         valor += 5;
+        
+        JsonObject result = new JsonObject();
+        result.addProperty("valor", valor);
+
+        return result;
+    }
+
+    @GetMapping(route = "/sub")
+    public JsonObject sub(JsonObject json){
+        int valor = 5;
         
         JsonObject result = new JsonObject();
         result.addProperty("valor", valor);
